73647df42c71acf6c6af40176f8491b65492810e
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-03-04 22:09:57 +0000

    finish sendToServer function

--- script.js
+++ script.js
@@ -22,4 +22,12 @@ const userInputInHTML = (input) => {
 const sendToServer = () => {
     const input = document.querySelector("#userinput").value;
     userInputInHTML(input); 
+
+    fetch("https://localhost:3000/secret", {
+        method: "POST",
+        body: JSON.stringify({ userInput: input }),
+        headers: new Headers({
+            "Content-Type": "application/json"
+        })
+    });
 };
